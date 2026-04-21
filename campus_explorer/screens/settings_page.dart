691fb6a5c82610b6eddbd4f7be933ebfc015a03b
mdswadhin-design <swadhinmiah310@gmail.com>
2026-04-21 11:00:49 +0600

    settings and logout page added

---
+++ campus_explorer/screens/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings'), backgroundColor: Colors.indigo),
+      body: const Center(child: Text('Settings Page (Coming Soon)', style: TextStyle(fontSize: 20))),
+    );
+  }
+}
